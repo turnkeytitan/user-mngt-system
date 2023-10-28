2c1721f76260c3e8c0f4b38ef94915187c770b66
Samuel Sanchez <samuel.sanchez@pragma.com.co>
2023-10-27 20:59:05 -0500

    chore: add eslint in compatibility with prettier feat: add login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/login/login.component')
+      .then(mod => mod.LoginComponent)
+  },
+];
