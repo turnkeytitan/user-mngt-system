3220c55496436104debd9371b40dffcdedc39fc9
Samuel Sanchez <sm.sanchez.co@gmail.com>
2023-10-28 20:20:28 -0500

    chore: add firestore feat: add remaining components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,31 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./home/home.component').then((mod) => mod.HomeComponent),
     ...canActivate(() => redirectUnauthorizedTo(['/login'])),
+    children: [
+      {
+        path: 'products',
+        loadComponent: () =>
+          import('./home/products/products.component').then(
+            (m) => m.ProductsComponent,
+          ),
+      },
+      {
+        path: 'inventory',
+        loadComponent: () =>
+          import('./home/inventory/inventory.component').then(
+            (m) => m.InventoryComponent,
+          ),
+      },
+      {
+        path: 'sales',
+        loadComponent: () =>
+          import('./home/sales/sales.component').then((m) => m.SalesComponent),
+      },
+      {
+        path: '**',
+        redirectTo: 'products',
+      },
+    ],
   },
   {
     path: '**',
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
